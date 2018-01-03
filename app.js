485fb561513427a61764668195bfa094980dd981
Margo Schaedel <margoschaedel@gmail.com>
2018-01-03 12:03:24 -0700

    Update styles

--- app.js
+++ app.js
@@ -1,115 +1,37 @@
+const Influx = require('influx');
 const express = require('express');
-const app = express();
 const path = require('path');
-const _ = require('lodash');
-const Influx = require('influxdb-nodejs');
-const client = new Influx('http://127.0.0.1:8086/telegraf');
-const onHeaders = require('on-headers');
+const os = require('os');
 const bodyParser = require('body-parser');
+const app = express();
+const influx = new Influx.InfluxDB('http://127.0.0.1:8086/telegraf');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', 3000);
 
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Rickshaw Demo';
+influx.getMeasurements()
+  .then(names => console.log('My measurement names are: ' + names.join(', ')))
+  .then(() => {
+    app.listen(app.get('port'), () => {
+      console.log(`Listening on ${app.get('port')}.`);
+    });
+  })
+  .catch(error => console.log({ error }));
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-});
-
-// set the http stats schema
-// client.schema('http', {
-//   use: 'integer',
-//   code: 'integer',
-//   bytes: 'integer',
-//   url: 'string',
-// });
-// client.on('writeQueue', () => {
-//   // sync write queue if the length is 100
-//   if (client.writeQueueLength === 10) {
-//     client.syncWrite()
-//       .then(() => {
-//         console.info('sync write success');
-//       })
-//       .catch(console.error);
-//   }
-// });
-
-function httpStats(req, res, next) {
-  const start = Date.now();
-  onHeaders(res, () => {
-    const code = res.statusCode;
-    const use = Date.now() - start;
-    const method = req.method;
-    const bytes = parseInt(res.get('Content-Length') || 0, 10);
-    const tags = {
-      spdy: _.sortedIndex([100, 300, 1000, 3000], use),
-      type: code / 100 | 0,
-      method,
-    };
-    const fields = {
-      use,
-      code,
-      bytes,
-      url: req.url,
-      route: req.route.path
-    };
-    // use queue for better performance
-    client.write('http')
-      .tag(tags)
-      .field(fields)
-      .queue();
-  });
-  next();
-}
-
-client.query('cpu')
-  // .where('host', )
-  // .addFunction('mean', 'usage_idle')
-  .then(results => console.log(results.series[0]))
-  .catch(console.error);
-// => influx ql: select * from "http" where "spdy" = '1' and "use" >= 300 and ("method" = 'GET' or "method" = 'POST')
-
-// client.createDatabase().catch(err => {
-//   console.error('create database fail err:', err);
-// });
-
-// app.use(httpStats);
-//
-// app.use((req, res, next) => {
-//   setTimeout(next, _.random(0, 5000));
-// });
-
-app.get('/users/me', (req, res) => {
-  console.log('request went through');
-  res.status(200).json({
-    account: 'vicanso',
-    name: 'Tree Xie',
-  });
-});
-
-app.get('/book/:id', (req, res) => {
-  const {
-    id,
-  } = req.params;
-  res.json({
-    id: id,
-    name: 'my book',
-    author: 'vicanso',
-  });
-});
-
-app.get('/order/:id', (req, res) => {
-  res.status(400).json({
-    error: 'The id is not valid',
-  });
-});
-
-app.get('/author/:id', (req, res) => {
-  res.status(500).json({
-    error: 'The database is disconnected',
-  });
+app.get('/api/v1/usage', (request, response) => {
+  influx.query(`
+    select mean("usage_user") as "mean_usage_user",
+    mean("usage_system") as "mean_usage_system" from cpu
+    where time > now() - 1h and
+    host = ${Influx.escape.stringLit(os.hostname())}
+    group by time(10s)
+    order by time desc
+    limit 200
+    `)
+    .then(result => response.status(200).json(result))
+    .catch(error => response.status(500).json({ error }));
 });
